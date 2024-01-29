51a8b16d33cf37a1209439291ed26848357c0a19
Ally Andrews <103804676+allyandrews94@users.noreply.github.com>
2024-01-29 13:01:33 -0500

    Update setup.sh to use Python 3.9 instead, as 3.8 was not working

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,19 +4,19 @@ echo " Setting up Capstone Environment"
 echo "****************************************"
 
 sudo apt-get update
-echo "Installing Python 3.8 Virtual Environment"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
+echo "Installing Python 3.9 Virtual Environment"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.9-venv
 
-echo "Making Python 3.8 the default..."
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+echo "Making Python 3.9 the default..."
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9
 
 
 
 echo "Checking the Python version..."
-python3 --version
+python3.9 --version
 
 echo "Creating a Python virtual environment"
-python3 -m venv ~/backend-songs-venv
+python3.9 -m venv ~/backend-songs-venv
 
 echo "Configuring the developer environment..."
 echo "# Backend Capstone Project additions" >> ~/.bashrc
@@ -24,7 +24,7 @@ echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 
 echo "Installing Python depenencies..."
-source ~/backend-songs-venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/backend-songs-venv/bin/activate && python3.9 -m pip install --upgrade pip wheel
 source ~/backend-songs-venv/bin/activate && pip install -r requirements.txt
 echo "source ~/backend-songs-venv/bin/activate" >> ~/.bashrc
 
